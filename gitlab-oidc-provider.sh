18ec34476c09897b1c9c038ba22ae03092710b0f
adhithia <37793374+aditirvan@users.noreply.github.com>
2022-07-12 09:12:02 +0700

    Update gitlab-oidc-provider.sh

--- gitlab-oidc-provider.sh
+++ gitlab-oidc-provider.sh
@@ -2,13 +2,13 @@
 vault auth enable oidc
 
 vault write auth/oidc/config \
-oidc_client_id="$oidc_client_id" \
-oidc_client_secret="$oidc_client_secret" \
-default_role="demo" \
-oidc_discovery_url="$oidc_discovery_url"
+oidc_client_id="_oidc_client_id" \
+oidc_client_secret="_oidc_client_secret" \
+default_role="default" \
+oidc_discovery_url="_oidc_discovery_url"
 
 vault write auth/oidc/role/demo \
 user_claim="sub" \
-allowed_redirect_uris="$allowed_redirect_uris" \
+allowed_redirect_uris="_allowed_redirect_uris" \
 oidc_scopes="openid" \
-policies=demo
+policies=default
