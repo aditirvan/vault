0625a22300e2e475d85d51422a12d408a917e63b
adhithia <37793374+aditirvan@users.noreply.github.com>
2022-07-12 09:49:50 +0700

    Update gitlab-oidc-enable.sh

--- gitlab-oidc-enable.sh
+++ gitlab-oidc-enable.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 vault auth enable oidc
-
+sleep 5
 vault login _token
-
+sleep 5
 vault write auth/oidc/config \
 oidc_client_id="_oidc_client_id" \
 oidc_client_secret="_oidc_client_secret" \
 default_role="default" \
 oidc_discovery_url="_oidc_discovery_url"
-
+sleep 5
 vault write auth/oidc/role/default \
 user_claim="sub" \
 allowed_redirect_uris="_allowed_redirect_uris" \
